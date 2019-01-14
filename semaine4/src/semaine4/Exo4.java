55b44a1ce862d33a4d1e6103c4be45639f732833
Papytuc1 <kevinsanmartino@gmail.com>
2019-01-14 18:01:31 +0100

    Modif de certain exo

--- semaine4/src/semaine4/Exo4.java
+++ semaine4/src/semaine4/Exo4.java
@@ -1,18 +1,19 @@
 package semaine4;
 
-import java.util.Arrays;
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Exo4 {
 
 	public static void main(String[] args) {
 		Scanner scanner = new Scanner(System.in);
-		String[] email = new String[10];
+		String[] email = new String[2];
 		String domaine ="";
 		int orange =0;
 		int sfr = 0;
 		int gmail = 0;
 		int hotmail = 0;
+		
 		for (int i = 0; i<email.length;i++) {
 			email[i]= scanner.nextLine();
 		}
@@ -20,17 +21,18 @@ public class Exo4 {
 			String e = email[x];
 			int stock = email[x].indexOf("@");/* je trouve l'index de @ et je le stock 
 			dans une variable pour le reutiliser plus bas*/
+			int last = email[x].indexOf(".");
 			//System.out.println(e);
-			for(int j = stock;j<e.length();j++) {
+			for(int j = stock;j<last;j++) {
 				//System.out.println(e.charAt(j));
 				domaine += e.charAt(j);// je concaténe chaque lettre pour reformer un mot et pouvoir le comparer
 				//System.out.println(domaine);
 				if(domaine.equals("@hotmail")) {
 					hotmail++;
-					domaine = "";/* je réinitialise domaine a chaque foi qu'il 
+					domaine = "" ;/* je réinitialise domaine a chaque foi qu'il 
 					trouve pour qu'il puisse recomencer a compter*/
 				}
-				if (domaine.equals("@orange")) {
+				else if (domaine.equals("@orange")) {
 					orange++;
 					domaine = "";
 
