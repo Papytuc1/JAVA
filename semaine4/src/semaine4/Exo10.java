a5fe2db587b37220f24a6d55415fccec6f8eb42d
Papytuc1 <kevinsanmartino@gmail.com>
2019-01-16 14:08:57 +0100

    Modif exo 10 fibonacci je part maintenant de 1 1

--- semaine4/src/semaine4/Exo10.java
+++ semaine4/src/semaine4/Exo10.java
@@ -7,9 +7,9 @@ public class Exo10 {
 	public static void main(String[] args) {
 		Scanner scanner = new Scanner(System.in);
 		int pointEntree = scanner.nextInt();
-		int fibonacci1 = 0;
+		int fibonacci1 = 1;
 		int fibonacci2 = 1;
-		int sum;
+		int sum=0;
 
 		for (int i = 0;i<pointEntree;i++) {
 			sum = fibonacci1+fibonacci2;
@@ -20,7 +20,7 @@ public class Exo10 {
 			// fn-1+fn-2
 
 		}
-		System.out.println("le resultat de la position "+pointEntree+" sur la suite de fibonacci est "+fibonacci2);
+		System.out.println("le resultat de la position "+pointEntree+" sur la suite de fibonacci est "+fibonacci1);
 
 	}
 
