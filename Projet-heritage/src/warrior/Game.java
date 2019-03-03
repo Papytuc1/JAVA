6679c40b0dc5b494aa6cd71380a7876ae0b11c1c
Papytuc1 <papytuc@hotmail.fr>
2019-03-03 19:29:03 +0100

    modif de la class game et de la class pnj qui permet de vendre des popo

--- Projet-heritage/src/warrior/Game.java
+++ Projet-heritage/src/warrior/Game.java
@@ -3,6 +3,7 @@ package warrior;
 import java.util.Scanner;
 
 public class Game {
+	static Pnj pnj;
 	static Scanner scanner = new Scanner(System.in);
 	/*
 	 * methode qui permet de check si la reponse est bien oui ou non
@@ -23,6 +24,9 @@ public class Game {
 		}
 		return reponse;
 	}
+	/*
+	 * methode de choix du nom du l'avatar
+	 */
 	public static String choixNom() {
 		boolean verif = true;
 		String nom="";
@@ -36,6 +40,9 @@ public class Game {
 		}
 		return nom;
 	}
+	/*
+	 * methode de choix de classe qui return un avatar guerrier mage ou voleur
+	 */
 	public static Avatar choixClasse() {
 		String nom = choixNom();
 		System.out.println("Quel classe voulez vous ? Vous avez le choix entre Guerrier, Mage ou voleur");
@@ -57,6 +64,9 @@ public class Game {
 		}
 		return avatar;
 	}
+	/*
+	 * methode de combat des avatars qui met fin au cbt si l'un des deux est mort
+	 */
 	public static void combat(Avatar joueur,Avatar adversaire) {
 		boolean endGame =false;
 		while(!endGame) {
@@ -71,6 +81,9 @@ public class Game {
 
 		}
 	}
+	/*
+	 * methode qui permet de choisir son adversaire et de lancer le combat entre vous et l'adversaire
+	 */
 	public static void choixAdversaire(Avatar joueur) {
 		boolean verif = true;
 		System.out.println("Contre qui voulez vous combattre\nGuerrier, Mage,voleur");
@@ -91,6 +104,9 @@ public class Game {
 		}
 		combat(joueur, adversaire);
 	}
+	/*
+	 * methode de tour du joueur qui lui permet de ce battre ou de boire une potion
+	 */
 	public static void tourJoueur(Avatar joueur,Avatar adversaire) {
 		boolean verif = true;
 		System.out.println("C'est a "+joueur.getNom()+" de jouer. Que doit t'il faire ?");
@@ -113,6 +129,9 @@ public class Game {
 		}
 
 	}
+	/*
+	 * methode qui permet a l'ordi de faire des choix entre boire une popo ou ce battre
+	 */
 	public static void tourAdversaire(Avatar adversaire,Avatar joueur) {
 		System.out.println("C'est a "+adversaire.getNom()+" de jouer !! ");
 		if(adversaire.getVie()<(adversaire.getVieMax()*0.25f) && adversaire.getPotion()>0) {
@@ -122,7 +141,9 @@ public class Game {
 			adversaire.attaquer(joueur);
 		}
 	}
-
+/*
+ * permet de check si un adversaire est mort ou non return un boolean et de lancer le pillage
+ */
 	public static boolean checkMort(Avatar joueur,Avatar adversaire) {
 		boolean mort = false;
 		if(joueur.getVie()<1) {
@@ -135,6 +156,9 @@ public class Game {
 		}
 		return mort;
 	}
+	/*
+	 * methode qui permet au joueur de choisir entre aller chercher une popo ou ce battre
+	 */
 	public static void sceanario(Avatar joueur) {
 		String saisie;
 		boolean verif = true;
@@ -144,7 +168,7 @@ public class Game {
 			switch (saisie) {
 			case "combattre":choixAdversaire(joueur);verif=false;
 			break;
-			case "magasin":
+			case "magasin":pnj.saluer(joueur);
 				break;
 			default:
 				break;
@@ -153,6 +177,7 @@ public class Game {
 	}
 
 	public static void main(String[] args) {
+		pnj = new Pnj("Papytuc");
 		Avatar joueur = choixClasse();
 		sceanario(joueur);
 	}
--- Projet-heritage/src/warrior/Pnj.java
+++ Projet-heritage/src/warrior/Pnj.java
@@ -1,18 +1,40 @@
 package warrior;
 
-public class Pnj extends Humain{
+import java.util.Scanner;
 
+public class Pnj extends Humain{
+	String reponseString="";
 	public Pnj(String nom) {
 		super(nom);
 		
 	}
 	public void vendrePotion(Avatar client) {
-		System.out.println("Bonjour voicis un potion"+client.getNom());
+		System.out.println("Bonjour voicis une potion "+client.getNom());
 		client.setPotion(+1);
 		client.setGold(client.getGold()-20);
 	}
 	@Override
 	public void saluer(Avatar avatar) {
-		System.out.println("Bonjour voulez vous une potion ?"+avatar.getNom());
+		Scanner scanner=new Scanner(System.in);
+		boolean verif = true;
+		System.out.println("Bonjour je suis "+ super.getNom()+" le vendeur de popo voulez vous une potion(oui/non) ?"+avatar.getNom());
+		while (verif) {
+			reponseString=scanner.nextLine();
+		if(reponseString.equals("oui")&& avatar.getGold()>=20) {
+			vendrePotion(avatar);
+			verif=false;
+		}
+		else if(reponseString.equals("non")) {
+			System.out.println("Aurevoir");
+			verif=false;
+		}
+		else if(avatar.getGold()<20){
+			System.err.println("Vous n'avez pas assez d'argent");
+			verif=false;
+		}
+		else {
+			System.out.println("Je n'ai pas compris votre reponse recomencer votre saisie");
+		}
+		}
 	}
 }
